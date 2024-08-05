3e230d7179db4541fd4a51c2fe25a00983ba54b7
tomogoapp <chris.tomogo@gmail.com>
2024-08-04 19:38:21 -0600

    comments: add commentsd

--- src/migration/migration.controller.ts
+++ src/migration/migration.controller.ts
@@ -12,12 +12,32 @@ export class MigrationController {
   ) {}
 
   @Get('migrate')
+/**
+ * The function `migrateData` asynchronously migrates data using a file path and returns a success
+ * message.
+ * @param {string} filePath - The `filePath` parameter in the `migrateData` function is a query
+ * parameter that specifies the path to the file containing the data to be migrated.
+ * @returns The function `migrateData` is returning an object with a message property indicating that
+ * the data migration was completed successfully.
+ */
   async migrateData(@Query('filePath') filePath:string){
     await this.migrationService.migrateData(filePath)
     return {message: 'Data migration complete successfully'}
   }
 
   @Get('by-zipcode')
+/**
+ * This TypeScript function retrieves locations based on a given zipcode and populates the state
+ * information.
+ * @param {string} zipcode - The `getByZipcode` function is an asynchronous function that takes a
+ * `zipcode` parameter as a query parameter. The function queries the `locationModel` to find locations
+ * with the specified `d_codigo` (zipcode) and populates the `state` field for each location. Finally,
+ * it
+ * @returns The function `getByZipcode` is returning locations that match the provided `zipcode`
+ * parameter. The locations are retrieved from the database using the `locationModel` and filtered
+ * based on the `d_codigo` field matching the provided `zipcode`. The function also populates the
+ * `state` field for each location before returning the results.
+ */
   async getByZipcode(@Query('zipcode') zipcode: string) {
     const locations = await this.locationModel.find({ d_codigo: zipcode }).populate('state');
     return locations;
